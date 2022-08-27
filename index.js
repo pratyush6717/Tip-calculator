88fbc849fb0ccf4218686110dfb9981535c87137
Pratyush Kumar <pratyushsingh6710@gmail.com>
2022-08-27 12:57:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,80 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@500&display=swap"
-      rel="stylesheet"
-    />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css" type="text/css" />
-    <title>Tip Calculator</title>
-  </head>
-  <body>
-    <div class="wrapper">
-      <div class="container" id="topContainer">
-        <div class="title">Bill total</div>
-        <div class="inputContainer">
-          <span>$</span>
-          <input
-            onkeyup="calculateBill()"
-            type="text"
-            id="billTotalInput"
-            placeholder="0.00"
-          />
-        </div>
-      </div>
-      <div class="container">
-        <div class="title">Tip</div>
-        <div class="inputContainer">
-          <span>%</span>
-          <input
-            onkeyup="calculateBill()"
-            type="text"
-            id="tipInput"
-            placeholder="10"
-          />
-        </div>
-      </div>
-      <div class="container" id="bottom">
-        <div class="splitContainer">
-          <div class="title">People</div>
-          <div class="controls">
-            <span class="buttonContainer">
-              <button class="splitButton" onclick="increasePeople()">
-                <span class="buttonText">+</span>
-              </button>
-            </span>
-            <span class="splitAmount" id="numberOfPeople">1</span>
-            <span class="buttonContainer">
-              <button class="splitButton" onclick="decreasePeople()">
-                <span class="buttonText">-</span>
-              </button>
-            </span>
-          </div>
-        </div>
-        <div class="totalContainer">
-          <div class="title">Total per Person</div>
-          <div class="total" id="perPersonTotal">$0.00</div>
-        </div>
-      </div>
-    </div>
+/* 
+🌟 APP: Tip Calculator
 
-    <script type="text/javascript" src="script.js"></script>
-  </body>
-</html>
+These are the 3 functions you must use 👇
+========================================
+calculateBill()
+increasePeople()
+decreasePeople()
+
+These functions are hard coded in the HTML. So, you can't change their names.
+
+These are all the DIV ID's you're gonna need access to 👇
+========================================================
+#1 ID 👉 'billTotalInput' = User input for bill total
+#2 ID 👉 'tipInput' = User input for tip
+#3 ID 👉 'numberOfPeople' = Current number of people you're splitting the bill between
+#4 ID 👉 'perPersonTotal' = Total dollar value owed per person
+*/
+
+// Get global access to all inputs / divs here (you'll need them later 😘)
+// bill input, tip input, number of people div, and per person total div
+const billInput = document.getElementById('billTotalInput')
+const tipInput = document.getElementById('tipInput')
+const numberOfPeopleDiv = document.getElementById('numberOfPeople')
+const perPersonTotalDiv = document.getElementById('perPersonTotal')
+
+// Get number of people from number of people div
+let numberOfPeople = Number(numberOfPeopleDiv.innerText)
+
+// ** Calculate the total bill per person **
+const calculateBill = () => {
+  // get bill from user input & convert it into a number
+  const bill = Number(billInput.value)
+
+  // get the tip from user & convert it into a percentage (divide by 100)
+  const tipPercent = Number(tipInput.value) / 100
+
+  // get the total tip amount
+  const tipAmount = bill * tipPercent
+
+  // calculate the total (tip amount + bill)
+  const total = tipAmount + bill
+
+  // calculate the per person total (total divided by number of people)
+  const perPersonTotal = total / numberOfPeople
+
+  // update the perPersonTotal on DOM & show it to user
+  perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`
+}
+
+// ** Splits the bill between more people **
+const increasePeople = () => {
+  // increment the amount
+  numberOfPeople += 1
+
+  // update the DOM with the new number of people
+  numberOfPeopleDiv.innerText = numberOfPeople
+
+  // calculate the bill based on the new number of people
+  calculateBill()
+}
+
+// ** Splits the bill between fewer people **
+const decreasePeople = () => {
+  // guard clause
+  // if amount is 1 or less simply return
+  // (a.k.a you can't decrease the number of people to 0 or negative!)
+  if (numberOfPeople <= 1) {
+    return
+  }
+
+  // decrement the amount
+  numberOfPeople -= 1
+
+  // update the DOM with the new number of people
+  numberOfPeopleDiv.innerText = numberOfPeople
+
+  // calculate the bill based on the new number of people
+  calculateBill()
+}
